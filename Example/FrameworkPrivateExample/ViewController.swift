1e29df4643c2051fff8cd45d8b22c4e2e65adb4b
Andrzej Michnia <andrzej.michnia@digidentity.com>
2019-02-12 17:38:53 +0100

    Added example app sketch

---
+++ Example/FrameworkPrivateExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrameworkPrivateExample
+//
+//  Created by Andrzej Michnia on 12/02/2019.
+//  Copyright © 2019 GirAppe Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
